2f1803816ac71a8685ce789b2ea9c809879a5507
Sasha the Beaver <astarikov820@gmail.com>
2025-03-01 00:57:07 +0600

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] KeyCode keyOne;
+    [SerializeField] KeyCode keyTwo;
+    [SerializeField] Vector3 moveDirection;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(keyOne))
+        {
+            GetComponent<Rigidbody>().linearVelocity += moveDirection;
+        }
+        if (Input.GetKey(keyTwo))
+        {
+            GetComponent<Rigidbody>().linearVelocity -= moveDirection;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.CompareTag("Player") && other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
